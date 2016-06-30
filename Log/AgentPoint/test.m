5f12be2607bcc29ee68a302e0e05fc12046ee78a
takarot <xeno.pc@outlook.com>
2016-06-30 21:27:10 +0900

    notRS, kernel 20, 0.50.5

---
+++ Log/AgentPoint/test.m
@@ -0,0 +1,7 @@
+% plot(x0, y0);
+% xlabel('test x')
+% ylabel('test y')
+
+plot(reward0_sample, reward0_reward);
+xlabel('reward0_sample');
+ylabel('reward');
---
+++ Log/reward/reward_plot.m
@@ -0,0 +1,71 @@
+close all;
+
+%{
+figure(1);
+plot(reward0_sample, reward0_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward0.dat');
+
+figure(2);
+plot(reward1_sample, reward1_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward1.dat');
+
+figure(3);
+plot(reward2_sample, reward2_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward2.dat');
+
+figure(4);
+plot(reward3_sample, reward3_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward3.dat');
+
+figure(5);
+plot(reward4_sample, reward4_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward4.dat');
+
+figure(6);
+plot(reward5_sample, reward5_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward5.dat');
+
+figure(7);
+plot(reward6_sample, reward6_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward6.dat');
+
+figure(8);
+plot(reward7_trial, reward7_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward7.dat');
+
+figure(9);
+plot(reward8_trial, reward8_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward8.dat');
+
+figure(10);
+plot(reward9_trial, reward9_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('reward9.dat');
+%}
+
+figure(11);
+plot(rewardAve_trial, rewardAve_reward, 'LineWidth' , 2);
+xlabel('trial');
+ylabel('reward');
+title('rewardave0.dat');
+
+
